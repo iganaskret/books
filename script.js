6edc4a0199c1687a8dece736d08e38a27d971191
Ksawery Karczewski <koscisty@MacBook-Pro-Ksawery.local>
2019-02-21 11:52:40 +0100

    All images

--- script.js
+++ script.js
@@ -1,25 +1,29 @@
+ // Template constants
+
  const link = "https://spreadsheets.google.com/feeds/list/1AQBvg1C7bOtdJcQPhTgwYS0_4Pojq2fyz4X3EbZp4I8/od6/public/values?alt=json";
  const bookmark = document.querySelector("#bookmark");
+ const template = document.querySelector('template').content;
+ const main = document.querySelector('main');
+ const article = document.querySelector('article');
+ //const section = document.querySelector('section');
 
  function loadJSON(link) {
      fetch(link).then(e => e.json()).then(data => data.feed.entry.forEach(displayBooksData));
  }
 
- // Template constants
-
- const template = document.querySelector('template').content;
- const main = document.querySelector('main');
-
- const imgLink = "https://kea-alt-del.dk/t5/site/imgs/";
- //const section = document.querySelector('section');
 
  function displayBooksData(data) {
+
+     console.log(data);
+
      const clone = template.cloneNode('true');
      //clone.querySelector("p").textContent = data.gsx$img.$t;
-     clone.querySelector("img").src = "imgs/and.jpg";
+     clone.querySelector("img").src = "imgs/1.jpg";
+
+
      clone.querySelector("#title").textContent = data.gsx$title.$t;
      clone.querySelector("#author").textContent = data.gsx$author.$t;
-     clone.querySelector("#year").textContent = "(" + data.gsx$publishingyear.$t + ")";
+     clone.querySelector("#year").textContent = " (" + data.gsx$publishingyear.$t + ")";
      clone.querySelector("h3").textContent = data.gsx$shortdescription.$t
      //clone.querySelector("h5").textContent = data.gsx$price.$t
      //clone.querySelector("h6").textContent = data.gsx$shortdescription.$t
@@ -31,6 +35,18 @@
 
      main.appendChild(clone);
  }
+
+
+//document.getElementById("demo").addEventListener("mouseover", mouseOver);
+//document.getElementById("demo").addEventListener("mouseout", mouseOut);
+
+//unction mouseOver() {
+  //document.getElementById("demo").style.color = "red";
+//}
+
+//function mouseOut() {
+  //document.getElementById("demo").style.color = "black";
+//}
 
 
  loadJSON(link);
