50dfe6fc186d038939f19175c538a19ed5dfe1ed
Iga <iga.naskret@gmail.com>
2019-02-26 10:00:13 +0100

    tina's changes and genres

--- script.js
+++ script.js
@@ -19,27 +19,6 @@
      fetch(link).then(e => e.json()).then(data => data.feed.entry.forEach(displayBooksData));
  }
 
- // MODAL CATEGORIES //
-
- function loadCat(link) {
-     fetch(linkCat).then(e => e.json()).then(data => data.feed.entry.forEach(createCategories));
- }
-
- function createCategories(category) {
-     const newLi = document.createElement("li");
-     const newA = document.createElement("a")
-     newA.textContent = category.gsx$genres.$t;;
-     newA.href = "#";
-     newA.addEventListener("click", () => showCategory(category));
-     modalCatList.appendChild(newLi);
-     modalCatList.appendChild(newA);
- }
-
- function showCategory(category) {
-
- }
-
- // END OF MODAL CATEGORIES //
 
  function displayBooksData(data) {
      const clone = template.cloneNode('true');
@@ -84,6 +63,37 @@
 
      main.appendChild(clone);
  }
+
+ // MODAL CATEGORIES //
+
+ function loadCat(link) {
+     fetch(linkCat).then(e => e.json()).then(data => data.feed.entry.forEach(createCategories));
+ }
+
+ function createCategories(category) {
+     const newLi = document.createElement("li");
+     const newA = document.createElement("a")
+     newA.textContent = category.gsx$genres.$t;;
+     newA.href = "#";
+     newA.addEventListener("click", () => showCategory(category));
+     modalCatList.appendChild(newLi);
+     modalCatList.appendChild(newA);
+ }
+
+ function showCategory(category) {
+     // if the name of the category == name of the category in the article (h5) then display it, otherwise don't //
+      document.querySelectorAll("article").forEach(article =>{
+                if (article.genre == category) {
+                    article.style.display = "grid";
+                    console.log("jest taka książka");
+                } else {
+                    article.style.display = "none";
+                    console.log("ni mo");
+                };
+      })
+ };
+
+ // END OF MODAL CATEGORIES //
 
  loadJSON(link);
  loadCat(linkCat);
